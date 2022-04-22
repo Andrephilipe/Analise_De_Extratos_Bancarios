b24a516026e991a08220ffce7bb7300ee3561c61
André Cassiano <andre.p.cassiano@accenture.com>
2022-04-22 10:16:27 -0300

    Primeiros códigos do projeto

---
+++ Real-Word-Software-Development/drs/src/test/java/com/desenvolvimento/drs/DrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desenvolvimento.drs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
