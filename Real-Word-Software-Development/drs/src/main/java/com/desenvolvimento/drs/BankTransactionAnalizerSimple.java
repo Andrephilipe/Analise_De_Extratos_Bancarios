d78e2c1f7df366e850bef350daff5f396d57de6d
André Cassiano <andre.p.cassiano@accenture.com>
2022-04-22 12:09:23 -0300

    Refatorando o código

--- Real-Word-Software-Development/drs/src/main/java/com/desenvolvimento/drs/BankTransactionAnalizerSimple.java
+++ Real-Word-Software-Development/drs/src/main/java/com/desenvolvimento/drs/BankTransactionAnalizerSimple.java
@@ -4,6 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -11,17 +13,33 @@ public class BankTransactionAnalizerSimple {
 
     private static final String RESOURCES = "C:/Users/andre.p.cassiano/Java - Estudos da linguagem/desenvolvimento-real-de-software/analise_de_extratos_bancarios/Real-Word-Software-Development/drs/src/main/resources/";
     public static void main(final String[] args) throws Exception {
-    
-        final Path path = Paths.get(RESOURCES + "banck-extract.csv");
+        final BankStatementCVSParser bankStatementParser = new BankStatementCVSParser();
+        final String fileName = "banck-extract.csv";
+        final Path path = Paths.get(RESOURCES + fileName);
         final List<String> lines = Files.readAllLines(path);
-        double total = 0;
-        for(final String line : lines) {
-            String[] columns = line.split(",");
-            double amount = Double.parseDouble(columns[1]);
-            total += amount;
-        }
 
-        System.out.println("The total for all transactions is " + total );
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFromCSV(lines);
+        System.out.println("test" + calculateTotalAmount(bankTransactions));
+        System.out.println("test" + selectInMonth(bankTransactions, Month.JANUARY));
+
+    }
+    public static double calculateTotalAmount(final List<BankTransaction> bankTransactions) {
+        double total = 0d;
+        for(final BankTransaction bankTransaction: bankTransactions) {
+            total += bankTransaction.getAmount();
+        }
+        return total;
+    }
+
+    public static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions, final Month month) {
+
+        final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
+        for(final BankTransaction bankTransaction: bankTransactions) {
+            if(bankTransaction.getDate().getMonth() == month) {
+                bankTransactionsInMonth.add(bankTransaction);
+            }
+        }
+        return bankTransactionsInMonth;
     }
 }
 
--- Real-Word-Software-Development/drs/src/main/java/com/desenvolvimento/drs/versoes/BankTransactionAnalizerSimpleORIGIN.java
+++ Real-Word-Software-Development/drs/src/main/java/com/desenvolvimento/drs/versoes/BankTransactionAnalizerSimpleORIGIN.java
@@ -1,16 +1,13 @@
-package com.desenvolvimento.drs;
+package com.desenvolvimento.drs.versoes;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @SpringBootApplication
-public class BankTransactionAnalizerSimpleV2 {
+public class BankTransactionAnalizerSimpleORIGIN {
 
     private static final String RESOURCES = "C:/Users/andre.p.cassiano/Java - Estudos da linguagem/desenvolvimento-real-de-software/analise_de_extratos_bancarios/Real-Word-Software-Development/drs/src/main/resources/";
     public static void main(final String[] args) throws Exception {
@@ -18,19 +15,13 @@ public class BankTransactionAnalizerSimpleV2 {
         final Path path = Paths.get(RESOURCES + "banck-extract.csv");
         final List<String> lines = Files.readAllLines(path);
         double total = 0;
-        DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         for(final String line : lines) {
-            final String[] columns = line.split(",");
-            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
-
-            if(date.getMonth() == Month.JANUARY) {
-                final double amount = Double.parseDouble(columns[1]);
-                total += amount;
-            }
-
+            String[] columns = line.split(",");
+            double amount = Double.parseDouble(columns[1]);
+            total += amount;
         }
 
-        System.out.println("The total for all transactions is January" + total );
+        System.out.println("The total for all transactions is " + total );
     }
 }
 
